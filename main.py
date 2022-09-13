e8c3e6b87792c500e4332baeb40dab445dbfd73e
Milind <milindchaudhary80@gmail.com>
2022-09-13 19:35:59 +0200

    Add eyes cascade using haarcascade

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import numpy as np
 
 # creating a face cascade
 face_cascade =  cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# creating eyes cascade
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
